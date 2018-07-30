d3498ac82c37d65b8b2cda13162c4f5e9fff2a52
liuzehua <liuzehua@yingerfashion.com>
2018-07-30 17:49:06 +0800

    update

--- ch02.py
+++ ch02.py
@@ -131,11 +131,37 @@ def ch02_03():
     def get_quantile_count(group, q=0.5):
         group = group.sort_values(by='prop', ascending=False)
         return group.prop.cumsum().searchsorted(q)[0] + 1
-    diversity = top1000.groupby(['year', 'sex']).apply(get_quantile_count)
+    # diversity = top1000.groupby(['year', 'sex']).apply(get_quantile_count)
     #FutureWarning: 'year' is both an index level and a column label.
     #Defaulting to column, but this will raise an ambiguity error in a future version
-    diversity = diversity.unstack('sex')
+    # diversity = diversity.unstack('sex')
     # diversity.plot(title='Number of popular names in top 50%')
+    # plt.show()
+
+    get_last_letter = lambda x: x[-1]
+    last_letters = names.name.map(get_last_letter)
+    last_letters.name = 'last_letter'
+    table = names.pivot_table('births', index=last_letters, columns=['sex', 'year'], aggfunc=sum)
+    subtable = table.reindex(columns=[1910, 1960, 2010], level='year')
+    letter_prop = subtable / subtable.sum()
+    fig, axes = plt.subplots(2, 1, figsize=(10, 8))
+    # letter_prop['M'].plot(kind='bar', rot=0, ax=axes[0], title='Male')
+    # letter_prop['F'].plot(kind='bar', rot=0, ax=axes[1], title='Female', legend=False)
+    # plt.show()
+
+    letter_prop  = table / table.sum()
+    dny_ts = letter_prop.loc[['d', 'n', 'y'], 'M'].T
+    # dny_ts.plot()
+    # plt.show()
+
+    all_names = top1000.name.unique()
+    mask = np.array(['lesl' in x.lower() for x in all_names])
+    lesley_like = all_names[mask]
+    filtered= top1000[top1000.name.isin(lesley_like)]
+    # filtered.groupby('name').births.sum()
+    # table = filtered.pivot_table('births', index='year', columns='sex', aggfunc=sum)
+    # table = table.div(table.sum(1), axis=0)
+    # table.plot(style={'M': 'k-', 'F': 'k--'})
     # plt.show()
 
 ch02_03()
